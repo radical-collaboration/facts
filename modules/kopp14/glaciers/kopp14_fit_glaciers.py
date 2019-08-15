7a2a152363be2ec6f9d6efeab335744cf9693b61
ggg121 <gggarner121@gmail.com>
2019-08-15 19:39:53 -0400

    Finish K14 Glaciers Stage 2

--- modules/kopp14/glaciers/kopp14_fit_glaciers.py
+++ modules/kopp14/glaciers/kopp14_fit_glaciers.py
@@ -2,6 +2,7 @@ import numpy as np
 import pickle
 import os
 import argparse
+from cholcov import cholcov
 
 ''' kopp14_fit_glaciers.py
 
@@ -17,29 +18,63 @@ Output:
 
 def kopp14_fit_glaciers(data_file):
 	
-	# Load the configuration file
+	# Load the data file
 	try:
 		f = open(data_file, 'rb')
 	except:
 		print("Cannot open data file\n")
 	
-	# Extract the configuration variables
+	# Extract the data variables
 	my_config = pickle.load(f)
 	f.close()
 	
-	rcp_scenario = my_config["rcp_scenario"]
+	#rcp_scenario = my_config["rcp_scenario"]
 	projGIC = my_config["projGIC"]
 	projGICse = my_config["projGICse"]
 	projGICyrs = my_config["projGICyrs"]
 	projGICmodel = my_config["projGICmodel"]
 	
+	# Define the target years
+	targyears = np.arange(2010, 2101, 10)
 	
-	## Store the thermal expansion variables in a pickle
-	#output = {'ThermExpMean': ThermExpMean, 'ThermExpStd': ThermExpStd,\
-	#	'ThermExpYears': ThermExpYears,	'ThermExpN': ThermExpN}
-	#outfile = open(os.path.join(os.path.dirname(__file__), "kopp14_thermalexp_fit.pkl"), 'wb')
-	#pickle.dump(output, outfile)
-	#outfile.close()
+	# Big Assumption: Years available across all models are the same... 1851 - 2300
+	GICyears = projGICyrs[:,0]	
+	
+	# Initialize variables to hold the distribution information
+	meanGIC = np.full((projGIC.shape[1], len(targyears)), np.nan)
+	NGIC = np.full(len(targyears), np.nan)
+	T = np.full((projGIC.shape[1], projGIC.shape[1], len(targyears)), np.nan)
+	
+	for i in np.arange(0,len(targyears)):
+		
+		# Which target year are we working with
+		targyear = targyears[i]
+		
+		# Find the temporal indices that span targyear by 4 years either side
+		theseGICyearInds = np.flatnonzero((GICyears >= np.max((2000,targyear-4))) * (GICyears <= np.min((np.max(targyears), targyear+4))))
+		
+		# Take the mean of the GIC data over this time window
+		targ = np.mean(projGIC[theseGICyearInds,:,:], 0).T
+		targse = np.mean(projGICse[theseGICyearInds,:,:], 0).T
+		
+		# Find the number of good GIC models available (i.e. data in all regions over the time window)
+		inds = np.flatnonzero(~np.isnan(np.sum(targ, 1)))
+		NGIC[i] = len(inds)
+		
+		# Find the multi-model mean GIC
+		meanGIC[:,i] = np.mean(targ[inds,:], 0)
+		
+		# Combine the multi-model variance with the model standard error
+		covGIC = np.cov(targ[inds,:], rowvar=False)
+		meanGICse = np.mean(targse[inds,:], 0)
+		covGIC = covGIC + (np.diag(meanGICse)**2)
+		T[:,:,i] = cholcov(covGIC)
+	
+	# Store the thermal expansion variables in a pickle
+	output = {'meanGIC': meanGIC, 'T': T, 'NGIC': NGIC}
+	outfile = open(os.path.join(os.path.dirname(__file__), "kopp14_glaciers_fit.pkl"), 'wb')
+	pickle.dump(output, outfile)
+	outfile.close()
 
 
 if __name__ == '__main__':
