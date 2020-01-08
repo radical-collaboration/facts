26031cfe520f6010a856db29898f3f05bd377263
George Koubbe <gkoubbe@outlook.com>
2020-01-07 20:07:47 -0500

    updated setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+''' Setup script, only usable via pip. '''
+
+from setuptools import setup
+
+from setuptools import setup
+
+setup(
+    name='facts',
+    version='0.1.0',
+    description='Framework for Assessing Changes To Sea-level (FACTS)',
+    url='https://github.com/radical-collaboration/facts.git',
+    author='Gregory Garner',
+    author_email='gregory.garner@rutgers.edu',
+    license='unlicense',
+    packages=['facts'],
+    zip_safe=False
+)
