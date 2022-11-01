6ccb73b528bfb33f27381a226b2035a13837acfd
Bob Kopp <robert.kopp@rutgers.edu>
2022-11-01 12:46:49 -0400

    rename modules

--- scripts/moduletest/run_workflowtest.sh
+++ scripts/moduletest/run_workflowtest.sh
@@ -4,8 +4,8 @@ MODULETEST_RELATIVE_DIR=$(dirname "${BASH_SOURCE[0]}")
 cd $MODULETEST_RELATIVE_DIR
 MODULETEST_DIR=`pwd`
 FACTSROOT_DIR=$MODULETEST_DIR/../..
-TOTALOUTPUT_GLOBAL_DIR=$FACTSROOT_DIR/modules/total/test/data_global
-TOTALOUTPUT_LOCAL_DIR=$FACTSROOT_DIR/modules/total/test/data_local
+TOTALOUTPUT_GLOBAL_DIR=$FACTSROOT_DIR/modules/facts/total/test/data_global
+TOTALOUTPUT_LOCAL_DIR=$FACTSROOT_DIR/modules/facts/total/test/data_local
 
 unset WORKDIR
 $FACTSROOT_DIR/modules/kopp14/verticallandmotion/test/run_moduletest.sh
@@ -29,12 +29,12 @@ else
 fi
 cd $TOTALOUTPUT_GLOBAL_DIR
 
-ln -s ../../../ssp/landwaterstorage/test/output/*globalsl.nc
-ln -s ../../../ipccar6/ismipemuicesheet/test/output/*WAIS_globalsl.nc
-ln -s ../../../ipccar6/ismipemuicesheet/test/output/*EAIS_globalsl.nc
-ln -s ../../../ipccar6/ismipemuicesheet/test/output/*GIS_globalsl.nc
-ln -s ../../../ipccar6/gmipemuglaciers/test/output/*globalsl.nc
-ln -s ../../../tlm/oceandynamics/test/output/*globalsl.nc
+ln -s ../../../../ssp/landwaterstorage/test/output/*globalsl.nc
+ln -s ../../../../ipccar6/ismipemuicesheet/test/output/*WAIS_globalsl.nc
+ln -s ../../../../ipccar6/ismipemuicesheet/test/output/*EAIS_globalsl.nc
+ln -s ../../../../ipccar6/ismipemuicesheet/test/output/*GIS_globalsl.nc
+ln -s ../../../../ipccar6/gmipemuglaciers/test/output/*globalsl.nc
+ln -s ../../../../tlm/oceandynamics/test/output/*globalsl.nc
 
 if ! [[ -d $TOTALOUTPUT_LOCAL_DIR ]]; then
     mkdir $TOTALOUTPUT_LOCAL_DIR
@@ -43,10 +43,10 @@ else
 fi
 cd $TOTALOUTPUT_LOCAL_DIR
 
-ln -s ../../../kopp14/verticallandmotion/test/output/*localsl.nc .
-ln -s ../../../ssp/landwaterstorage/test/output/*localsl.nc
-ln -s ../../../ipccar6/ismipemuicesheet/test/output/*WAIS_localsl.nc
-ln -s ../../../ipccar6/ismipemuicesheet/test/output/*EAIS_localsl.nc
-ln -s ../../../ipccar6/ismipemuicesheet/test/output/*GIS_localsl.nc
-ln -s ../../../ipccar6/gmipemuglaciers/test/output/*localsl.nc
-ln -s ../../../tlm/oceandynamics/test/output/*localsl.nc
+ln -s ../../../../kopp14/verticallandmotion/test/output/*localsl.nc .
+ln -s ../../../../ssp/landwaterstorage/test/output/*localsl.nc
+ln -s ../../../../ipccar6/ismipemuicesheet/test/output/*WAIS_localsl.nc
+ln -s ../../../../ipccar6/ismipemuicesheet/test/output/*EAIS_localsl.nc
+ln -s ../../../../ipccar6/ismipemuicesheet/test/output/*GIS_localsl.nc
+ln -s ../../../../ipccar6/gmipemuglaciers/test/output/*localsl.nc
+ln -s ../../../../tlm/oceandynamics/test/output/*localsl.nc
