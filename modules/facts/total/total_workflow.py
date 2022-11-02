d8a0f24cf6c789581d2396f49c1a0825bc9d2f4c
Bob Kopp <robert.kopp@rutgers.edu>
2022-11-02 12:21:10 -0400

    update total_workflow to allow specification of single workflow/scale from workflows.yml

--- modules/facts/total/total_workflow.py
+++ modules/facts/total/total_workflow.py
@@ -15,7 +15,7 @@ def TotalSamplesInDirectory(directory, pyear_start, pyear_end, pyear_step, chunk
 	outdir = os.path.dirname(__file__)
 
 	# Define the output file
-	outfilename = "total-workflow.nc"
+	outfilename = "total.workflow.nc"
 	outfile = os.path.join(outdir, outfilename)
 
 	# Get the list of input files
@@ -37,13 +37,31 @@ def TotalSamplesInDirectory(directory, pyear_start, pyear_end, pyear_step, chunk
 	return(r)
 
 
-def TotalSamplesInWorkflows(directory, pyear_start, pyear_end, pyear_step, chunksize):
+def TotalSampleInWorkflow(wfcfg, directory, targyears, workflow, scale, chunksize=50):
+		# Define the output file
+		outdir = os.path.dirname(__file__)
+		outfilename = "total.workflow." + workflow + "." + scale + ".nc"
+		outfile = os.path.join(outdir, outfilename)
+
+		# Get the list of input files
+		infiles = []
+		r=[]
+		for this_file in wfcfg[workflow][scale]:
+			infiles.append(os.path.join(directory,this_file))
+
+		if len(infiles)>0:
+			rout=TotalSamples(infiles, outfile, targyears, chunksize)
+			r.append(rout)
+
+			# Put a copy of the total file back into the shared directory
+			shutil.copy2(outfile, directory)
+		return(r)
+
+
+def TotalSamplesInWorkflows(directory, pyear_start, pyear_end, pyear_step, chunksize, workflow=[""], scale=[""]):
 
 	# Define the years of interest
 	targyears = xr.DataArray(np.arange(pyear_start, pyear_end+1, pyear_step), dims="years")
-
-	# Output directory
-	outdir = os.path.dirname(__file__)
 
 	# read yaml file
 	wfcfg_file = os.path.join(directory, "workflows.yml")
@@ -51,26 +69,20 @@ def TotalSamplesInWorkflows(directory, pyear_start, pyear_end, pyear_step, chunk
 		wfcfg = yaml.safe_load(fp)
 
 	# loop through workflows and scopes
-	breakpoint()
 	r=[]
 	for this_workflow in wfcfg:
 		for this_scale in wfcfg[this_workflow]:
-
-			# Define the output file
-			outfilename = "total_workflow_" + this_workflow + "_" + this_scale + ".nc"
-			outfile = os.path.join(outdir, outfilename)
-
-			# Get the list of input files
-			infiles = []
-			for this_file in wfcfg[this_workflow][this_scale]:
-				infiles.append(os.path.join(directory,this_file))
-
-			if len(infiles)>0:
-				rout=TotalSamples(infiles, outfile, targyears, chunksize)
-				r.append(rout)
-
-				# Put a copy of the total file back into the shared directory
-				shutil.copy2(outfile, directory)
+			if len(workflow[0])>0:
+				if this_workflow in workflow:
+					if len(scale[0])>0:
+						if this_scale in scale:
+							r.append(TotalSampleInWorkflow(wfcfg, directory, targyears, this_workflow, this_scale, chunksize))
+						else:
+							r.append(TotalSampleInWorkflow(wfcfg, directory, targyears, this_workflow, this_scale, chunksize))
+					else:
+						r.append(TotalSampleInWorkflow(wfcfg, directory, targyears, this_workflow, this_scale, chunksize))
+			else:
+				r.append(TotalSampleInWorkflow(wfcfg, directory, targyears, this_workflow, this_scale, chunksize))
 
 	return(r)
 
@@ -130,6 +142,8 @@ if __name__ == "__main__":
 	# Define the command line arguments to be expected
 	parser.add_argument('--directory', help="Directory containing files to aggregate", required=True)
 	parser.add_argument('--workflows', help="Use a workflows.yml file listing files to aggregate", action="store_true")
+	parser.add_argument('--workflow', help="Workflow to run", type=str, default="")
+	parser.add_argument('--scale', help="Scale to run", type=str, default="")
 	parser.add_argument('--pyear_start', help="Year for which projections start [default=2020]", default=2020, type=int)
 	parser.add_argument('--pyear_end', help="Year for which projections end [default=2100]", default=2100, type=int)
 	parser.add_argument('--pyear_step', help="Step size in years between pyear_start and pyear_end at which projections are produced [default=10]", default=10, type=int)
@@ -138,10 +152,11 @@ if __name__ == "__main__":
 	# Parse the arguments
 	args = parser.parse_args()
 
-	# Total up the workflow in the provided directory
-	if args.workflows:
-		TotalSamplesInWorkflows(args.directory, args.pyear_start, args.pyear_end, args.pyear_step, args.chunksize)
+	if args.workflows or (len(args.workflow)>0) or (len(args.scale)>0):
+		# Total up by workflow
+		TotalSamplesInWorkflows(args.directory, args.pyear_start, args.pyear_end, args.pyear_step, args.chunksize, workflow=[args.workflow], scale=[args.scale])
 	else:
+		# Total up the workflow in the provided directory
 		TotalSamplesInDirectory(args.directory, args.pyear_start, args.pyear_end, args.pyear_step, args.chunksize)
 
 	exit()
