34140eb5ebcad62f3ccf8ba235f2dfad9f9debb0
Bob Kopp <robert.kopp@rutgers.edu>
2022-11-08 15:36:46 -0500

    working on emulandice

--- modules/emulandice/shared/emulandice_environment.sh
+++ modules/emulandice/shared/emulandice_environment.sh
@@ -7,5 +7,9 @@
 
 module use /projects/community/modulefiles
 module load intel/19.0.3
-module load R/4.1.0-gc563
+module load R/3.6.3-gc563
+module load gcc/5.4
 
+if [ "$1" == "--Rscript" ]; then
+    Rscript -e "source('packratc/init.R')" -e "packrat::install_local('emulandice_1.1.0.tar.gz')"
+fi
--- modules/emulandice/shared/emulandice_steer.sh
+++ modules/emulandice/shared/emulandice_steer.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
-if [[ -d results ]]
+if [[ ! -d results ]]; then
     mkdir results
 fi
 
-Rscript -e "source('packrat/init.R')"
-
-# have to do something special for dummies since it's been removed
-Rscript -e 'packrat::set_opts(local.repos = "packrat/src")' -e 'packrat::install_local("dummies/dummies_1.5.6.tar.gz")'
-
-# restore other packages
-Rscript -e "source('packrat/init.R')" -e 'packrat::restore()'
-
-# install emulandice package
-Rscript -e 'packrat::set_opts(local.repos = "packrat/src/emulandice")' -e 'packrat::install_local("emulandice_1.1.0.tar.gz")'
-
 # run emulandice
-emulandice_dataset = $1
-N_FACTS=$2
-Rscript -e 'library(emulandice)' -e "main('decades',dataset=$emulandice_dataset,N_FACTS=$N_FACTS)"
+emulandice_dataset=$1
+Rscript -e "emulandice::main('decades',dataset=\'$emulandice_dataset\')"
