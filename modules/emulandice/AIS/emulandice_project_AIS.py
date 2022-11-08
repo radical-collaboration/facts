13d368b56477edc11b2ed10dea42b67745cb4550
Bob Kopp <robert.kopp@rutgers.edu>
2022-11-08 17:10:09 -0500

    emulandice/AIS

--- modules/emulandice/AIS/emulandice_project_AIS.py
+++ modules/emulandice/AIS/emulandice_project_AIS.py
@@ -83,7 +83,7 @@ def ExtractProjections(emulandice_file):
 	return(wais_data, eais_data, pen_data, targyears)
 
 
-def emulandice_project_AIS(pipeline_id, nsamps):
+def emulandice_project_AIS(pipeline_id, nsamps, icesource="AIS"):
 
 	# Load the preprocessed data
 	preprocess_file = "{}_preprocess.pkl".format(pipeline_id)
@@ -120,7 +120,6 @@ def emulandice_project_AIS(pipeline_id, nsamps):
 
 	py_working_dir = os.getcwd()
 	emulandice_dataset = 'FACTS_CLIMATE_FORCING.csv'
-	icesource='AIS'
 	subprocess.run(["bash", "emulandice_steer.sh", emulandice_dataset, str(nsamps), icesource])
 
 	# Get the output from the emulandice run
@@ -218,12 +217,13 @@ if __name__ == "__main__":
 	# Add arguments for the resource and experiment configuration files
 	parser.add_argument('--pipeline_id', help="Unique identifier for this instance of the module", required=True)
 	parser.add_argument('--nsamps', help="Number of samples to draw (default = 10000)", default=10000, type=int)
+	parser.add_argument('--icesource', help="Ice source to run (AIS, GrIS, glaciers)", choices=['AIS','GrIS','Glaciers'],default='AIS')
 
 	# Parse the arguments
 	args = parser.parse_args()
 
 	# Run the preprocessing
-	emulandice_project_AIS(args.pipeline_id, args.nsamps)
+	emulandice_project_AIS(args.pipeline_id, args.nsamps, args.icesource)
 
 	# Done
 	sys.exit()
