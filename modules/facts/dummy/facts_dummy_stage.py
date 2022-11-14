d06c30d85b981eda991a8e7d998ec025782e558b
Bob Kopp <robert.kopp@rutgers.edu>
2022-11-13 22:47:22 -0500

    added hooks to facilitate experiment step bypass

---
+++ modules/facts/dummy/facts_dummy_stage.py
@@ -0,0 +1,28 @@
+import numpy as np
+import csv
+import argparse
+import pickle
+import os
+
+''' facts_dummy_stage.py
+
+This module does nothing, but provides a convenient module to use for
+moving files around on the server as specified in a custom pipeline.yml
+file. (For example, this could be used to place climate data files
+from a previous run in the $SHARED/climate directory.)
+
+'''
+
+if __name__ == '__main__':
+	
+	# Initialize the command-line argument parser
+	parser = argparse.ArgumentParser(description="Run the facts/dummy module stage.",\
+	epilog="Note: This is meant to be run within the Framework for the Assessment of Changes To Sea-level (FACTS)")
+	
+	# Define the command line arguments to be expected
+	parser.add_argument('--pipeline_id', help="Unique identifier for this instance of the module")
+
+	# Parse the arguments
+	args = parser.parse_args()
+	
+	exit()
