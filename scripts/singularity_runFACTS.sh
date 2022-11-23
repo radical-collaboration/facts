6629bb830ffce9d70f77af471606c4eaa6c2fbc4
Bob Kopp <robert.kopp@rutgers.edu>
2022-11-23 12:02:34 -0500

    update singularity_runFACTS

--- scripts/singularity_runFACTS.sh
+++ scripts/singularity_runFACTS.sh
@@ -7,12 +7,17 @@ if [ ! -d $LOGDIR ]; then
     mkdir $LOGDIR
 fi
 
-singularity run -w $HOME/singularity/rabbitmq > $LOGDIR/rabbitmq.`hostname`.out 2> $LOGDIR/rabbitmq.`hostname`.err &
-singularity run -w $HOME/singularity/mongo > $LOGDIR/mongo.`hostname`.out 2> $LOGDIR/mongo.`hostname`.err &
+singularity instance start -w $HOME/singularity/rabbitmq rabbitmq
+singularity instance start -w $HOME/singularity/mongo mongo
+
+singularity run -w instance://rabbitmq > $LOGDIR/rabbitmq.`hostname`.out 2> $LOGDIR/rabbitmq.`hostname`.err &
+singularity run -w instance://mongo > $LOGDIR/mongo.`hostname`.out 2> $LOGDIR/mongo.`hostname`.err &
+
+sleep 30
 
 j=0
 for i in $@
 do
-    python $FACTSDIR/runFACTS.py $i > $LOGDIR/experiment.$j.`hostname`.out 2> $LOGDIR/experiment.$j.`hostname`.err   
+    python $FACTSDIR/runFACTS.py $i > $LOGDIR/experiment.$j.`hostname`.out 2> $LOGDIR/experiment.$j.`hostname`.err
     j=$(($j + 1))
 done
