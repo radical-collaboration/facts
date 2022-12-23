9fa98964985fd624116ea5c2e7df5c11fe1a7b43
Bob Kopp <bobkopp@users.noreply.github.com>
2022-12-22 22:36:03 -0500

    Update singularity_runFACTS.sh

--- scripts/singularity_runFACTS.sh
+++ scripts/singularity_runFACTS.sh
@@ -3,6 +3,18 @@
 FACTSDIR=$HOME/facts
 LOGDIR=$FACTSDIR/log
 
+if [ ! -d $HOME/singularity ]; then
+    mkdir $HOME/singularity
+fi
+
+if [ ! -d $HOME/singularity/mongo ]; then
+    mkdir $HOME/singularity/mongo
+fi
+
+if [ ! -f $HOME/singularity/mongo/singularity ]; then
+    singularity build --sandbox $HOME/singularity/mongo docker://mongo
+fi
+
 if [ ! -d $LOGDIR ]; then
     mkdir $LOGDIR
 fi
