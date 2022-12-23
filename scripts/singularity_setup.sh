904e727c9deffbc25027b57ae6c0f75bbb7cfa1d
Bob Kopp <bobkopp@users.noreply.github.com>
2022-12-22 22:35:06 -0500

    Update singularity_setup.sh

--- scripts/singularity_setup.sh
+++ scripts/singularity_setup.sh
@@ -8,15 +8,6 @@ if [ ! -d $HOME/singularity/mongo ]; then
     mkdir $HOME/singularity/mongo
 fi
 
-#if [ ! -d $HOME/singularity/rabbitmq ]; then
-#    mkdir $HOME/singularity/rabbitmq
-#fi
-
 if [ ! -f $HOME/singularity/mongo/singularity ]; then
     singularity build --sandbox $HOME/singularity/mongo docker://mongo
 fi
-
-
-#if [ ! -f $HOME/singularity/rabbitmq/singularity ]; then
-#    singularity build --sandbox $HOME/singularity/rabbitmq docker://rabbitmq
-#fi
