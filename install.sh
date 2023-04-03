802e3094641e13462a4f9d4717011f0578e8aca8
Alexander Reedy <ar2208@amarel2.amarel.rutgers.edu>
2023-04-03 14:03:34 -0400

    Swapped Deconto16 postprocess with Deconto21 postprocess

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-
-pwd
+if [[ "$VIRTUAL_ENV" != "" ]]
+then
+    echo "venv is active $VIRTUAL_ENV"
+    echo "collecting package requirments..."
+    pip freeze
+else
+    echo "Please activate your venv"
+fi
--- modules/deconto16/AIS/deconto16_AIS_postprocess.py
+++ modules/deconto16/AIS/deconto16_AIS_postprocess.py
@@ -5,20 +5,21 @@ import pickle
 import time
 import argparse
 import re
-from netCDF4 import Dataset
 from read_locationfile import ReadLocationFile
 from AssignFP import AssignFP
 
+import xarray as xr
+import dask.array as da
 
 ''' dp16_postprocess_icesheet.py
 
-This script runs the ice sheet post-processing task for the DP16 workflow. This task
-uses the global projections from the 'dp16_project_icesheet' script and applies
+This script runs the ice sheet post-processing task for the DP20 workflow. This task
+uses the global projections from the 'dp21_project_icesheet' script and applies
 spatially resolved fingerprints to the ice sheet contribution. The result is a netCDF4
-file that contains spatially and temporally resolved samples of ice sheet contributions 
+file that contains spatially and temporally resolved samples of ice sheet contributions
 to local sea-level rise
 
-Parameters: 
+Parameters:
 locationfile = File that contains points for localization
 pipeline_id = Unique identifer for the pipeline running this code
 
@@ -26,8 +27,8 @@ Output: NetCDF file containing local contributions from ice sheets
 
 '''
 
-def dp16_postprocess_icesheet(locationfilename, pipeline_id):
-	
+def dp16_postprocess_icesheet(locationfilename, chunksize, pipeline_id):
+
 	# Read in the fitted parameters from parfile
 	projfile = "{}_projections.pkl".format(pipeline_id)
 	try:
@@ -35,96 +36,90 @@ def dp16_postprocess_icesheet(locationfilename, pipeline_id):
 	except:
 		print("Cannot open projfile\n")
 		sys.exit(1)
-	
-	# Load the site locations	
-	locationfile = os.path.join(os.path.dirname(__file__), locationfilename)
-	(_, site_ids, site_lats, site_lons) = ReadLocationFile(locationfile)
-	
+
 	# Extract the data from the file
 	my_data = pickle.load(f)
 	waissamps = my_data['wais_samps']
 	eaissamps = my_data['eais_samps']
 	targyears = my_data['targyears']
 	scenario = my_data['scenario']
+	baseyear = my_data['baseyear']
 	f.close()
-	
+
+	# Load the site locations
+	locationfile = os.path.join(os.path.dirname(__file__), locationfilename)
+	(_, site_ids, site_lats, site_lons) = ReadLocationFile(locationfile)
+
+	# Get some dimension data from the loaded data structures
+	nsamps = eaissamps.shape[0]
+	nyears = len(targyears)
+	nsites = len(site_ids)
+
 	# Get the fingerprints for all sites from all ice sheets
 	fpdir = os.path.join(os.path.dirname(__file__), "FPRINT")
-	waisfp = AssignFP(os.path.join(fpdir,"fprint_wais.nc"), site_lats, site_lons)
-	eaisfp = AssignFP(os.path.join(fpdir,"fprint_eais.nc"), site_lats, site_lons)
-	
-	# Multiply the fingerprints and the projections
+	waisfp = da.from_array(AssignFP(os.path.join(fpdir,"fprint_wais.nc"), site_lats, site_lons), chunks=chunksize)
+	eaisfp = da.from_array(AssignFP(os.path.join(fpdir,"fprint_eais.nc"), site_lats, site_lons), chunks=chunksize)
+
+	# Rechunk the fingerprints for memory
+	#waisfp = waisfp.rechunk(chunksize)
+	#eaisfp = eaisfp.rechunk(chunksize)
+
+	# Apply the fingerprints to the projections
 	waissl = np.multiply.outer(waissamps, waisfp)
 	eaissl = np.multiply.outer(eaissamps, eaisfp)
-	
-	# Write to netcdf
-	writeNetCDF(waissl, pipeline_id, "WAIS", targyears, site_lats, site_lons, site_ids, scenario)
-	writeNetCDF(eaissl, pipeline_id, "EAIS", targyears, site_lats, site_lons, site_ids, scenario)
-	writeNetCDF(waissl + eaissl, pipeline_id, "AIS", targyears, site_lats, site_lons, site_ids, scenario)
-	
-	return(0)
+
+	# Add up the east and west components for AIS total
+	aissl = waissl + eaissl
+
+	# Define the missing value for the netCDF files
+	nc_missing_value = np.iinfo(np.int16).min
+
+	# Create the xarray data structures for the localized projections
+	ncvar_attributes = {"description": "Local SLR contributions from icesheets according to DP21 workflow",
+			"history": "Created " + time.ctime(time.time()),
+			"source": "SLR Framework: DP21 workflow",
+			"scenario": scenario,
+			"baseyear": baseyear}
+
+	wais_out = xr.Dataset({"sea_level_change": (("samples", "years", "locations"), waissl, {"units":"mm", "missing_value":nc_missing_value}),
+							"lat": (("locations"), site_lats),
+							"lon": (("locations"), site_lons)},
+		coords={"years": targyears, "locations": site_ids, "samples": np.arange(nsamps)}, attrs=ncvar_attributes)
+
+	eais_out = xr.Dataset({"sea_level_change": (("samples", "years", "locations"), eaissl, {"units":"mm", "missing_value":nc_missing_value}),
+							"lat": (("locations"), site_lats),
+							"lon": (("locations"), site_lons)},
+		coords={"years": targyears, "locations": site_ids, "samples": np.arange(nsamps)}, attrs=ncvar_attributes)
+
+	ais_out = xr.Dataset({"sea_level_change": (("samples", "years", "locations"), aissl, {"units":"mm", "missing_value":nc_missing_value}),
+							"lat": (("locations"), site_lats),
+							"lon": (("locations"), site_lons)},
+		coords={"years": targyears, "locations": site_ids, "samples": np.arange(nsamps)}, attrs=ncvar_attributes)
+
+	# Write the netcdf output files
+	wais_out.to_netcdf("{0}_{1}_localsl.nc".format(pipeline_id, "WAIS"), encoding={"sea_level_change": {"dtype": "i2", "zlib": True, "complevel":4, "_FillValue": nc_missing_value}})
+	eais_out.to_netcdf("{0}_{1}_localsl.nc".format(pipeline_id, "EAIS"), encoding={"sea_level_change": {"dtype": "i2", "zlib": True, "complevel":4, "_FillValue": nc_missing_value}})
+	ais_out.to_netcdf("{0}_{1}_localsl.nc".format(pipeline_id, "AIS"), encoding={"sea_level_change": {"dtype": "i2", "zlib": True, "complevel":4, "_FillValue": nc_missing_value}})
+
+	return(None)
 
 
-def writeNetCDF(data, pipeline_id, icesheet_name, targyears, site_lats, site_lons, site_ids, scenario):
-		
-	# Write the localized projections to a netcdf file
-	rootgrp = Dataset(os.path.join(os.path.dirname(__file__), "{0}_{1}_localsl.nc".format(pipeline_id, icesheet_name)), "w", format="NETCDF4")
-
-	# Define Dimensions
-	nsites = len(site_ids)
-	ntimes = len(targyears)
-	nsamps = data.shape[1]
-	year_dim = rootgrp.createDimension("years", ntimes)
-	samp_dim = rootgrp.createDimension("samples", nsamps)
-	loc_dim  = rootgrp.createDimension("locations", nsites)
-	
-	# Populate dimension variables
-	year_var = rootgrp.createVariable("years", "i4", ("years",))
-	samp_var = rootgrp.createVariable("samples", "i8", ("samples",))
-	loc_var  = rootgrp.createVariable("locations", "i8", ("locations",))
-	lat_var  = rootgrp.createVariable("lat", "f4", ("locations",))
-	lon_var  = rootgrp.createVariable("lon", "f4", ("locations",))
-
-	# Create a data variable
-	samps = rootgrp.createVariable("sea_level_change", "f4", ("samples", "years", "locations"), zlib=True, least_significant_digit=2)
-
-	# Assign attributes
-	rootgrp.description = "Local SLR contributions from {} icesheet according to DP16 workflow".format(icesheet_name)
-	rootgrp.history = "Created " + time.ctime(time.time())
-	rootgrp.source = "SLR Framework: DP16 workflow"
-	rootgrp.scenario = scenario
-	lat_var.units = "Degrees North"
-	lon_var.units = "Degrees East"
-	samps.units = "mm"
-
-	# Put the data into the netcdf variables
-	lat_var[:]   = site_lats
-	lon_var[:]   = site_lons
-	loc_var[:]   = site_ids
-	year_var[:]  = targyears
-	samp_var[:]  = np.arange(nsamps)
-	samps[:,:,:] = np.transpose(data[:,:,:],(1,0,2))
-
-	# Close the netcdf
-	rootgrp.close()
-	
-	return(0)
-	
 if __name__ == '__main__':
-	
+
 	# Initialize the command-line argument parser
 	parser = argparse.ArgumentParser(description="Run the post-processing stage for the DP16 icesheet SLR projection workflow",\
 	epilog="Note: This is meant to be run as part of the Framework for the Assessment of Changes To Sea-level (FACTS)")
-	
-	# Define the command line arguments to be expected	
+
+	# Define the command line arguments to be expected
 	parser.add_argument('--locationfile', help="File that contains name, id, lat, and lon of points for localization", default="location.lst")
+	parser.add_argument('--chunksize', help="Number of locations to process at a time [default=50]", type=int, default=50)
 	parser.add_argument('--pipeline_id', help="Unique identifier for this instance of the module")
-	
+
 	# Parse the arguments
 	args = parser.parse_args()
-	
+
 	# Run the postprocessing for the parameters specified from the command line argument
-	dp16_postprocess_icesheet(args.locationfile, args.pipeline_id)
-	
+	dp16_postprocess_icesheet(args.locationfile, args.chunksize, args.pipeline_id)
+
 	# Done
 	exit()
