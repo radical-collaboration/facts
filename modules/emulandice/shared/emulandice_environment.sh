0a1e3c3d87630811201180b833065c5a97461fce
Alexander Reedy <ar2208@amarel1.amarel.rutgers.edu>
2023-05-22 12:41:52 -0400

    Merged with FACTS emulandice setup fix from main

--- modules/emulandice/shared/emulandice_environment.sh
+++ modules/emulandice/shared/emulandice_environment.sh
@@ -3,16 +3,19 @@
 # this script loads the modules
 # required for emulandice to run on Amarel
 #
-# You will have to modify it for your system.
+# You may have to modify it for your system.
 
-module use /projects/community/modulefiles
+hs=`hostname`
+if [ ${hs:0:6} = 'amarel' ]; then
+    module use /projects/community/modulefiles
 
-module purge
-module load intel/19.0.3
-module load R/3.6.3-gc563
-module load gcc/7.3.0-gc563
+    module purge
+    module load intel/19.0.3
+    module load R/3.6.3-gc563
+    module load gcc/7.3.0-gc563
 
-module list
+    module list
+fi
 
 if [ "$1" == "--Rscript" ]; then
     Rscript -e "source('packrat/init.R')" -e "packrat::install_local('emulandice_1.1.0.tar.gz')"
