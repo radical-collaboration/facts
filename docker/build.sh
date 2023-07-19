8a362f397f741017535d7b356c788f49e6f172d4
AHReedy <alexander.h.reedy@gmail.com>
2023-07-19 14:53:59 -0400

    Initial commit for FACTS Docker container creation

---
+++ docker/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BASE_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )/../" &> /dev/null && pwd 2> /dev/null; )"
+
+TAG="factsealevel/facts:$TAG"
+
+if [[ -z $PLATFORM ]]; then
+    PLATFORM="linux/amd64"
+fi
+
+echo "Build docker container $TAG (name: ${NAME:-n/a})"
+docker build \
+    -t "$TAG" \
+    --build-arg TUTORIAL_NAME="$NAME" \
+    --build-arg BUILDPLATFORM="$PLATFORM" \
+    -f "$RCT_BASE_DIR/docker/Dockerfile" \
+    "$RCT_BASE_DIR/src/"
