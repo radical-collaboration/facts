b1b80d742588b00bf80f1036f1b2310422825f9c
Bob Kopp <robert.kopp@rutgers.edu>
2023-10-04 15:22:12 -0400

    prototype interpolation scheme

--- modules/bamber19/icesheets/bamber19_icesheets_preprocess.py
+++ modules/bamber19/icesheets/bamber19_icesheets_preprocess.py
@@ -17,75 +17,25 @@ pipeline_id = Unique identifier for the pipeline running this code
 '''
 
 def bamber19_preprocess_icesheets(pyear_start, pyear_end, pyear_step, baseyear, scenario, pipeline_id):
-	
-	scenario_map = {"rcp85": 'corefileH', "rcp26": 'corefileL',  \
-					"tlim2.0win0.25": 'corefileL', "tlim5.0win0.25": 'corefileH'}
-	this_corefile = scenario_map[scenario]
-	
+
 	# Define the target years
 	targyears = np.arange(pyear_start, pyear_end+1, pyear_step)
-	
+
 	# Load the data
 	filename = os.path.join(os.path.dirname(__file__), "SLRProjections190726core_SEJ_full.mat")
 	mat = scipy.io.loadmat(filename)
 	
-	wais_samps, eais_samps, gis_samps = ExtractSamples(mat, this_corefile, targyears, baseyear)
-	OutputData(pipeline_id, eais_samps, wais_samps, gis_samps, scenario, targyears, baseyear)
-
-def bamber19_preprocess_icesheets_temperaturedriven(pyear_start, pyear_end, pyear_step, baseyear, scenario, climate_data_file, pipeline_id):
-	# Load the temperature data
-	SAT,Time,NumTensemble = GetSATData(climate_data_file, scenario)
-
-	# Rest of code needs to be written
-	print('This does not work yet')
-
-	wais_sampsH, eais_sampsH, gis_sampsH = ExtractSamples(mat, "corefileH", targyears, baseyear)
-	wais_sampsL, eais_sampsL, gis_sampsL = ExtractSamples(mat, "corefileL", targyears, baseyear)
-
-	# For each temperature time series sample, compute a score in 2050 and 2100 that compares to the Bamber
-	# low and high scenarios (in 2050, low=1.5 C and high=2.0 C; in 2100, 2 C and 5 C, all relative to 1850-1900)
-	# -- ie so score = 0 if below Low and 1 of above High, and linearly interpolated in between.
-	# Average together 2050 and 2100 scores, then use score as probability of drawing from the High distribution
-	# (as opposed to Low distribution) for that time series.
-
-	# for each temperature sample you pick which scenario to use based on comparing the weighting factor to a random draw
-
-	OutputData(pipeline_id, eais_samps, wais_samps, gis_samps, scenario, targyears, baseyear)
-
-def GetSATData(fname, scenario, refyear_start=1850, refyear_end=1900, year_start=1900, year_end=2300):
-
-	# Open the file
-	df_ssp = h5py.File(fname,'r')
-
-	# Extract the surface temperature for this scenario
-	if scenario not in df_ssp.keys():
-		raise Except("Scenario {} not found in {}".format(scenario, fname))
-	ssp_folder = df_ssp.get(scenario)
-	sat_ssp = ssp_folder.get('surface_temperature')
-
-	# Get the number of ensemble members from the data
-	_,nens = sat_ssp.shape
-
-	# Extract the years available
-	sat_years = df_ssp['year'][()]
-
-	# Which indices align with the reference and trim years
-	refyear_start_idx = np.flatnonzero(sat_years == refyear_start)[0]
-	refyear_end_idx = np.flatnonzero(sat_years == refyear_end)[0] #+ 1
-	year_start_idx = np.flatnonzero(sat_years == year_start)[0]
-	year_end_idx = np.flatnonzero(sat_years == year_end)[0] + 1
-
-	# Normalize and crop temperature series
-	Time = np.arange(year_start, year_end+1)
-	SATave = np.mean(sat_ssp[refyear_start_idx:refyear_end_idx,:], axis=0)
-	SAT = sat_ssp[year_start_idx:year_end_idx,:] - SATave
-
-	# Close the h5 file
-	df_ssp.close()
-
-	# Done
-	return(SAT, Time, nens)
-
+	if scenario=='both':
+		wais_sampsH, eais_sampsH, gis_sampsH = ExtractSamples(mat, 'corefileH', targyears, baseyear)
+		wais_sampsL, eais_sampsL, gis_sampsL = ExtractSamples(mat, 'corefileH', targyears, baseyear)
+		OutputDataAll(pipeline_id, eais_sampsH, wais_sampsH, gis_sampsH,  eais_sampsL, wais_sampsL, gis_sampsL, targyears, baseyear)
+	else:
+		scenario_map = {"rcp85": 'corefileH', "rcp26": 'corefileL',  \
+						"tlim2.0win0.25": 'corefileL', "tlim5.0win0.25": 'corefileH'}
+		this_corefile = scenario_map[scenario]
+		wais_samps, eais_samps, gis_samps = ExtractSamples(mat, this_corefile, targyears, baseyear)
+		OutputDataScen(pipeline_id, eais_samps, wais_samps, gis_samps, scenario, targyears, baseyear)
+	
 def ExtractSamples(mat, this_corefile, targyears, baseyear):
 	# Get the years available from the matlab core file
 	mat_years = np.squeeze(mat[this_corefile][0,0][27])
@@ -119,7 +69,7 @@ def ExtractSamples(mat, this_corefile, targyears, baseyear):
 	return wais_samps, eais_samps, gis_samps
 
 
-def OutputData(pipeline_id, eais_samps, wais_samps, gis_samps, scenario, targyears, baseyear):
+def OutputDataScen(pipeline_id, eais_samps, wais_samps, gis_samps, scenario, targyears, baseyear):
 
 	# Sum up the components to get total AIS samples
 	ais_samps = eais_samps + wais_samps
@@ -136,6 +86,25 @@ def OutputData(pipeline_id, eais_samps, wais_samps, gis_samps, scenario, targyea
 	p.dump(outdata, outfile)
 	outfile.close()
 
+
+def OutputDataAll(pipeline_id, eais_sampsH, wais_sampsH, gis_sampsH,  eais_sampsL, wais_sampsL, gis_sampsL, scenario, targyears, baseyear):
+
+	# Sum up the components to get total AIS samples
+	ais_sampsH = eais_sampsH + wais_sampsH
+	ais_sampsL = eais_sampsL + wais_sampsL
+
+	# Populate the output dictionary
+	outdata = {'eais_sampsH': eais_sampsH, 'wais_sampsH': wais_sampsH, 'ais_sampsH': ais_sampsH, \
+				'gis_sampsH': gis_sampsH, 'eais_sampsL': eais_sampsL, 'wais_sampsL': wais_sampsL, \
+				'ais_sampsL': ais_sampsL, 'gis_sampsL': gis_sampsL, 'targyears': targyears, 'baseyear': baseyear}
+	
+	# Define the data directory
+	outdir = os.path.dirname(__file__)
+
+	# Write the rates data to a pickle file
+	outfile = open(os.path.join(outdir, "{}_data.pkl".format(pipeline_id)), 'wb')
+	p.dump(outdata, outfile)
+	outfile.close()
 
 def FindRefVals(timeseries, years, baseyear):
 	
@@ -162,17 +131,16 @@ if __name__ == '__main__':
 	parser.add_argument('--pyear_end', help="Projection year end [default=2100]", default=2100, type=int)
 	parser.add_argument('--pyear_step', help="Projection year step [default=10]", default=10, type=int)
 	parser.add_argument('--scenario', help="Emissions scenario of interest [default=rcp85]", choices=["rcp85", "rcp26", "tlim2.0win0.25", "tlim5.0win0.25"], default="rcp85")
-	parser.add_argument('--climate_data_file', help="NetCDF4/HDF5 file containing surface temperature data", type=str, default="")
 	parser.add_argument('--baseyear', help="Year to which projections are referenced [default = 2000]", default=2000, type=int)
+	parser.add_argument('--climate_data_file', help="NetCDF4/HDF5 file containing surface temperature data", type=str, default="")
 	
 	# Parse the arguments
 	args = parser.parse_args()
 	
 	if len(args.climate_data_file) == 0:
-		# Run the preprocessing stage with the user defined RCP scenario
 		bamber19_preprocess_icesheets(args.pyear_start, args.pyear_end, args.pyear_step, args.baseyear, args.scenario, args.pipeline_id)
 	else:
-		bamber19_preprocess_icesheets_temperaturedriven(args.pyear_start, args.pyear_end, args.pyear_step, args.baseyear, args.scenario, args.climate_data_file, args.pipeline_id)
+		bamber19_preprocess_icesheets(args.pyear_start, args.pyear_end, args.pyear_step, args.baseyear, 'both', args.pipeline_id)
 
 	# Done
 	sys.exit()
--- modules/bamber19/icesheets/bamber19_icesheets_project.py
+++ modules/bamber19/icesheets/bamber19_icesheets_project.py
@@ -68,6 +68,60 @@ def bamber19_project_icesheets(nsamps, pipeline_id, replace, rngseed):
 	return(0)
 
 
+def bamber19_project_icesheets_temperaturedriven(climate_data_file, pipeline_id, replace, rngseed):
+
+	# Load the data file
+	datafilename = "{}_data.pkl".format(pipeline_id)
+	datafile = os.path.join(os.path.dirname(__file__), datafilename)
+
+	with open(datafile, 'rb') as f:
+		my_data = pickle.load(f)
+
+	years = my_data["targyears"]
+	scenario = my_data["scenario"]
+	baseyear = my_data["baseyear"]
+	ais_samplesH = my_data["ais_sampsH"]
+	eais_samplesH = my_data["eais_sampsH"]
+	wais_samplesH = my_data["wais_sampsH"]
+	gis_samplesH = my_data["gis_sampsH"]
+	ais_samplesL = my_data["ais_sampsL"]
+	eais_samplesL = my_data["eais_sampsL"]
+	wais_samplesL = my_data["wais_sampsL"]
+	gis_samplesL = my_data["gis_sampsL"]
+
+	# Generate the sample indices
+	np.random.seed(rngseed)
+	sample_inds = np.random.choice(ais_samples.shape[0], size=nsamps, replace=replace)
+	useHigh=pickScenario(climate_data_file, scenario, rngseed);
+
+	# Store the samples for AIS components
+	eais_samps = eais_samplesL[sample_inds,:]
+	wais_samps = wais_samplesL[sample_inds,:]
+	ais_samps = ais_samplesL[sample_inds,:]
+	gis_samps = gis_samplesL[sample_inds,:]
+
+	eais_samps[useHigh,:] = eais_samplesH[sample_inds[useHigh],:]
+	wais_samps[useHigh,:] = wais_samplesH[sample_inds[useHigh],:]
+	ais_samps[useHigh,:] = ais_samplesH[sample_inds[useHigh],:]
+	gis_samps[useHigh,:] = gis_samplesH[sample_inds[useHigh],:]
+
+    # Store the variables in a pickle
+	output = {'eais_samps': eais_samps, 'wais_samps': wais_samps, \
+				'ais_samps': ais_samps, 'gis_samps': gis_samps, 'years': years, \
+				'scenario': scenario, 'baseyear': baseyear}
+	outfilename = "{}_projections.pkl".format(pipeline_id)
+	outfile = open(os.path.join(os.path.dirname(__file__), outfilename), 'wb')
+	pickle.dump(output, outfile)
+	outfile.close()
+
+	# Write the projections to the netCDF files
+	WriteNetCDF(pipeline_id, eais_samps, years, nsamps, "EAIS", scenario, baseyear)
+	WriteNetCDF(pipeline_id, wais_samps, years, nsamps, "WAIS", scenario, baseyear)
+	WriteNetCDF(pipeline_id, ais_samps, years, nsamps, "AIS", scenario, baseyear)
+	WriteNetCDF(pipeline_id, gis_samps, years, nsamps, "GIS", scenario, baseyear)
+
+	return(0)
+
 def WriteNetCDF(pipeline_id, global_samps, years, nsamps, ice_source, scenario, baseyear):
 
 	# Write the total global projections to a netcdf file
@@ -108,6 +162,55 @@ def WriteNetCDF(pipeline_id, global_samps, years, nsamps, ice_source, scenario, 
 	# Close the netcdf
 	rootgrp.close()
 
+
+def GetSATData(fname, scenario, refyear_start=1850, refyear_end=1900, year_start=1900, year_end=2300):
+
+	# Open the file
+	df_ssp = h5py.File(fname,'r')
+
+	# Extract the surface temperature for this scenario
+	if scenario not in df_ssp.keys():
+		raise Except("Scenario {} not found in {}".format(scenario, fname))
+	ssp_folder = df_ssp.get(scenario)
+	sat_ssp = ssp_folder.get('surface_temperature')
+
+	# Get the number of ensemble members from the data
+	_,nens = sat_ssp.shape
+
+	# Extract the years available
+	sat_years = df_ssp['year'][()]
+
+	# Which indices align with the reference and trim years
+	refyear_start_idx = np.flatnonzero(sat_years == refyear_start)[0]
+	refyear_end_idx = np.flatnonzero(sat_years == refyear_end)[0] #+ 1
+	year_start_idx = np.flatnonzero(sat_years == year_start)[0]
+	year_end_idx = np.flatnonzero(sat_years == year_end)[0] + 1
+
+	# Normalize and crop temperature series
+	Time = np.arange(year_start, year_end+1)
+	SATave = np.mean(sat_ssp[refyear_start_idx:refyear_end_idx,:], axis=0)
+	SAT = sat_ssp[year_start_idx:year_end_idx,:] - SATave
+
+	# Close the h5 file
+	df_ssp.close()
+
+	# Done
+	return(SAT, Time, nens)
+
+def pickScenario(climate_data_file, scenario, rngseed)
+	# Load the temperature data
+	SAT,Time,NumTensemble = GetSATData(climate_data_file, scenario)
+	x2=np.where(Time[:] == 2100)[0][0]
+	SAT2=SAT[x2,:]
+	f2=np.minimum(1,np.maximum(0,(SAT2-2)/(5-2)))
+	weights=f2
+
+	# Select which scenario to draw from for each sample
+	np.random.seed(rngseed)
+	selector = np.random.rand(SAT2.size)
+	useHigh = (selector<weights)
+	return useHigh
+
 if __name__ == '__main__':
 
 	# Initialize the command-line argument parser
@@ -119,11 +222,16 @@ if __name__ == '__main__':
 	parser.add_argument('--replace', help="Allow sampling with replacement (default = 1)", choices=(0,1), type=int, default=1)
 	parser.add_argument('--seed', help="Seed for the random number generator (default = 1234)", default=1234, type=int)
 	parser.add_argument('--pipeline_id', help="Unique identifier for this instance of the module")
+	parser.add_argument('--climate_data_file', help="NetCDF4/HDF5 file containing surface temperature data", type=str, default="")
+
 
 	# Parse the arguments
 	args = parser.parse_args()
 
-	# Run the projection stage with the provided arguments
-	bamber19_project_icesheets(args.nsamps, args.pipeline_id, args.replace, args.seed)
+	if len(args.climate_data_file) == 0:
+		# Run the preprocessing stage with the user defined RCP scenario
+		bamber19_project_icesheets(args.nsamps, args.pipeline_id, args.replace, args.seed)
+	else:
+		bamber19_project_icesheets_temperaturedriven(args.climate_data_file,args.pipeline_id, args.replace, args.seed)
 
 	exit()
