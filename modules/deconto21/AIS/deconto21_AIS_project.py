18deb429a808b14434ee428abfd0214e5326a748
Bob Kopp <robert.kopp@rutgers.edu>
2023-10-14 18:56:47 -0400

    prototype interpolation

--- modules/deconto21/AIS/deconto21_AIS_project.py
+++ modules/deconto21/AIS/deconto21_AIS_project.py
@@ -3,6 +3,7 @@ import argparse
 import pickle
 import sys
 import os
+import h5py
 from netCDF4 import Dataset
 import time
 
@@ -24,17 +25,7 @@ of this module within the same workflow.
 def dp21_project_icesheet(nsamps, pyear_start, pyear_end, pyear_step, pipeline_id, replace, rngseed):
 
 	# Load the data file
-	datafilename = "{}_data.pkl".format(pipeline_id)
-	datafile = os.path.join(os.path.dirname(__file__), datafilename)
-
-	with open(datafile, 'rb') as f:
-		my_data = pickle.load(f)
-
-	years = my_data["years"]
-	wais = my_data["wais_samps"]
-	eais = my_data["eais_samps"]
-	scenario = my_data["scenario"]
-	baseyear = my_data["baseyear"]
+	years, wais, eais, scenario, baseyear = LoadDataFile(pipeline_id)
 
 	# Define the target projection years
 	targyears = np.arange(pyear_start, pyear_end+1, pyear_step)
@@ -53,25 +44,145 @@ def dp21_project_icesheet(nsamps, pyear_start, pyear_end, pyear_step, pipeline_i
 	wais_samps = wais[datayr_idx[:,np.newaxis],sample_idx[np.newaxis,:]]
 	eais_samps = eais[datayr_idx[:,np.newaxis],sample_idx[np.newaxis,:]]
 
+	WriteOutput(eais_samps, wais_samps, targyears[targyear_idx], scenario, pipeline_id,baseyear):
+
+	return(None)
+
+
+def dp21_project_icesheet_temperatueredriven(climate_data_file, pyear_start, pyear_end, pyear_step, pipeline_id, replace, rngseed):
+
+	# Load the data file
+	years, wais, eais, scenario, baseyear = LoadDataFile(pipeline_id)
+
+	# Define the target projection years
+	targyears = np.arange(pyear_start, pyear_end+1, pyear_step)
+
+	# Extract the pool size from the data
+	pool_size = eais.shape[1]
+
+	# Find the data years that overlap with the target projection years
+	(_, datayr_idx, targyear_idx) = np.intersect1d(years, targyears, return_indices = True)
+
+	# Generate the sample indices
+	np.random.seed(rngseed)
+	sample_idx = np.random.choice(pool_size, size=nsamps, replace=replace)
+
+	# Store the samples
+	wais_samps0 = wais[datayr_idx[:,np.newaxis],sample_idx[np.newaxis,:],:]
+	eais_samps0 = eais[datayr_idx[:,np.newaxis],sample_idx[np.newaxis,:],:]
+
+	# identify which samples to draw from which scenario
+	useScenario=pickScenario(climate_data_file, scenario);
+
+	# Store the samples for AIS components
+	eais_samps = eais_samps0[:,:,0]
+	wais_samps = wais_samps0[:,:,0]
+
+	for ii in range(1,2):
+		eais_samps[:,useScenario==ii] = eais_samps0[:,:,ii]
+		wais_samps[:,useScenario==ii] = wais_samps0[:,:,ii]
+
+	WriteOutput(eais_samps, wais_samps, targyears[targyear_idx], scenario, pipeline_id,baseyear):
+	return(None)
+
+
+def GetSATData(fname, scenario, refyear_start=1850, refyear_end=1900, year_start=1900, year_end=2300):
+
+	# Open the file
+	df_ssp = h5py.File(fname,'r')
+
+	# Extract the surface temperature for this scenario
+	if scenario not in df_ssp.keys():
+		raise Except("Scenario {} not found in {}".format(scenario, fname))
+	ssp_folder = df_ssp.get(scenario)
+	sat_ssp = ssp_folder.get('surface_temperature')
+
+	# Get the number of ensemble members from the data
+	_,nens = sat_ssp.shape
+
+	# Extract the years available
+	sat_years = df_ssp['year'][()]
+
+	# Which indices align with the reference and trim years
+	refyear_start_idx = np.flatnonzero(sat_years == refyear_start)[0]
+	refyear_end_idx = np.flatnonzero(sat_years == refyear_end)[0] #+ 1
+	year_start_idx = np.flatnonzero(sat_years == year_start)[0]
+	year_end_idx = np.flatnonzero(sat_years == year_end)[0] + 1
+
+	# Normalize and crop temperature series
+	Time = np.arange(year_start, year_end+1)
+	SATave = np.mean(sat_ssp[refyear_start_idx:refyear_end_idx,:], axis=0)
+	SAT = sat_ssp[year_start_idx:year_end_idx,:] - SATave
+
+	# Close the h5 file
+	df_ssp.close()
+
+	# Done
+	return(SAT, Time, nens)
+
+def pickScenario(climate_data_file, scenario):
+	# Load the temperature data
+	
+	SAT,Time,NumTensemble = GetSATData(climate_data_file, scenario)
+
+	# find integrated SAT over 2000-2099
+	x2=np.where((Time[:]<2100) * (Time[:]>=2000))
+	SAT2=SAT[x2]
+	iSAT=SAT2.sum(axis=0)
+	selector = np.random.rand(iSAT.size)
+
+	# convert integrated temperature into a normalized variable between low and high scenarios
+
+	# Based on CCSM temperature projections used in the paper, integrated
+	# 21st century temperature is about 133, 167 and 245 C*yr for RCP 2.6, 4.5 and 8.5,
+	# respectively.
+
+	iSAT_marker = np.array([133,167,245])
+
+	f1=np.minimum(1,np.maximum(0,(iSAT-iSAT_marker[0])/(iSAT_marker[1]-iSAT_marker[0])))
+	f2=np.minimum(1,np.maximum(0,(iSAT-iSAT_marker[1])/(iSAT_marker[2]-iSAT_marker[1])))
+
+	# Select which scenario to draw from for each sample
+	useScenario = (selector<f1)
+	useScenario[iSAT>iSAT_marker[1]] = 1 + (selector[iSAT>iSAT_marker[1]]<f2)
+	return useScenario
+
+def WriteOutput(eais_samps, wais_samps,  targyears, scenario, pipeline_id,baseyear):
+
 	# Transpose the samples to fit the output data structure
 	wais_samps = wais_samps.T
 	eais_samps = eais_samps.T
 	ais_samps = wais_samps + eais_samps
 
     # Store the variables in a pickle
+    # Store the variables in a pickle
 	output = {'eais_samps': eais_samps, 'wais_samps': wais_samps, \
-				'scenario': scenario, 'targyears': targyears[targyear_idx], 'baseyear': baseyear}
+				'scenario': scenario, 'targyears': targyears, 'baseyear': baseyear}
 	outfilename = "{}_projections.pkl".format(pipeline_id)
 	outfile = open(os.path.join(os.path.dirname(__file__), outfilename), 'wb')
 	pickle.dump(output, outfile)
 	outfile.close()
 
 	# Write the global projections to output netCDF files
-	WriteNetCDF(eais_samps, "EAIS", targyears[targyear_idx], scenario, pipeline_id, baseyear)
-	WriteNetCDF(wais_samps, "WAIS", targyears[targyear_idx], scenario, pipeline_id, baseyear)
-	WriteNetCDF(ais_samps, "AIS", targyears[targyear_idx], scenario, pipeline_id, baseyear)
+	WriteNetCDF(eais_samps, "EAIS", targyears, scenario, pipeline_id, baseyear)
+	WriteNetCDF(wais_samps, "WAIS", targyears, scenario, pipeline_id, baseyear)
+	WriteNetCDF(ais_samps, "AIS", targyears, scenario, pipeline_id, baseyear)
 
-	return(None)
+def LoadDataFile(pipeline_id):
+	# Load the data file
+	datafilename = "{}_data.pkl".format(pipeline_id)
+	datafile = os.path.join(os.path.dirname(__file__), datafilename)
+
+	with open(datafile, 'rb') as f:
+		my_data = pickle.load(f)
+
+	years = my_data["years"]
+	wais = my_data["wais_samps"]
+	eais = my_data["eais_samps"]
+	scenario = my_data["scenario"]
+	baseyear = my_data["baseyear"]
+
+	return years, wais, eais, scenario, baseyear
 
 
 def WriteNetCDF(icesamps, icetype, data_years, scenario, pipeline_id, baseyear):
@@ -133,11 +244,17 @@ if __name__ == '__main__':
 	parser.add_argument('--replace', help="Allow sampling with replacement [default = 1]", choices=('0','1'), default=1)
 	parser.add_argument('--seed', help="Seed for the random number generator [default = 1234]", default=1234, type=int)
 	parser.add_argument('--pipeline_id', help="Unique identifier for this instance of the module")
+	parser.add_argument('--climate_data_file', help="NetCDF4/HDF5 file containing surface temperature data", type=str, default="")
 
 	# Parse the arguments
 	args = parser.parse_args()
 
 	# Run the projection stage with the provided arguments
-	dp21_project_icesheet(args.nsamps, args.pyear_start, args.pyear_end, args.pyear_step, args.pipeline_id, args.replace, args.seed)
+	if len(args.climate_data_file) == 0:
+		# Run the preprocessing stage with the user defined RCP scenario
+		dp21_project_icesheet(args.nsamps, args.pyear_start, args.pyear_end, args.pyear_step, args.pipeline_id, args.replace, args.seed)
+	else:
+		dp21_project_icesheet_temperaturedriven(args.climate_data_file, args.pyear_start, args.pyear_end, args.pyear_step, args.pipeline_id, args.replace, args.seed)
+
 
 	exit()
