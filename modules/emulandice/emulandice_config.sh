e32cca4462c0768f152ff5d49c549dc13334de66
Bob Kopp <robert.kopp@rutgers.edu>
2023-12-14 08:43:58 -0500

    dont delete dummies

--- modules/emulandice/emulandice_config.sh
+++ modules/emulandice/emulandice_config.sh
@@ -7,7 +7,7 @@ cd $ROOTDIR/shared
 case $MODE in 
     "clean")
         echo Cleaning up old files...
-        rm -v *.gz* *.tgz
+        rm -v emulandice_bundled_dependencies.tgz emulandice_*gz
         ;;
     *)
         source emulandice_environment.sh
