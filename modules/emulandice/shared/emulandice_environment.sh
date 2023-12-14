77c2f1abe5111ab3aaf27859265fe6305f759f52
Bob Kopp <robert.kopp@rutgers.edu>
2023-12-14 09:47:46 -0500

    switch to R 3.6.3 on Amarel

--- modules/emulandice/shared/emulandice_environment.sh
+++ modules/emulandice/shared/emulandice_environment.sh
@@ -11,7 +11,7 @@ if [ ${hs: -18} = 'amarel.rutgers.edu' ]; then
 
     module purge
     module load gcc/10.2.0/openmpi
-    module load R/4.1.0-gc563
+    module load R/3.6.3-gc563
     module load cmake/3.24.3-sw1088
 
     module list
