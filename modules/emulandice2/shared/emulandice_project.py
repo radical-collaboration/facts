ca424a580636549fd6215f18dedfc14a015ccf4e
Bob Kopp <robert.kopp@rutgers.edu>
2023-12-20 10:03:46 -0500

    prototype streamlined emulandice2 project script

--- modules/emulandice2/shared/emulandice_project.py
+++ modules/emulandice2/shared/emulandice_project.py
@@ -1,210 +1,40 @@
-import numpy as np
-import os
-import sys
-import fnmatch
 import argparse
-import re
-import pickle
-import time
 import subprocess
-import shutil
-from netCDF4 import Dataset
-from scipy.stats import norm, truncnorm
 
-def ExtractProjections(emulandice_file):
-
-	# Initialize
-	ice_sources = []
-	regions = []
-	years = []
-	samples = []
-	sles = []
-
-	# Open the emulandice_file for reading
-	with open(emulandice_file, "r") as f:
-
-		# Skip the header line
-		_ = f.readline()
-
-		# Load the rest of the data
-		for line in f:
-			lp = re.split(",", line)
-			ice_sources.append(lp[0])
-			regions.append(lp[1])
-			years.append(int(lp[2]))
-			samples.append(int(lp[3]))
-			sles.append(float(lp[7]))
-
-	# Convert to numpy arrays
-	ice_sources = np.array(ice_sources)
-	regions = np.array(regions)
-	years = np.array(years)
-	samples = np.array(samples)
-	sles = np.array(sles)
-
-	# Extract the unique target years, samples, and regions
-	targyears = np.unique(years)
-	unique_samples = np.unique(samples)
-	unique_regions = np.unique(regions)
-
-	# Initialize the return data structure
-	ret_data = np.full((len(unique_regions), len(unique_samples), len(targyears)), np.nan)
-
-	# Pre-compile the regular expression that extracts region number
-	region_regex = re.compile("^region_(\d+)$")
-
-	# Loop over all the entries
-	for i in np.arange(len(sles)):
-
-		# This sample
-		this_icesource = ice_sources[i]
-		this_region = regions[i]
-		this_year = years[i]
-		this_sample = samples[i]
-		this_sle = sles[i]
-
-		# Skip this entry if it's not for AIS
-		if this_icesource != "Glaciers":
-			continue
-
-		# Which index will this entry be filling?
-		year_idx = np.flatnonzero(targyears == this_year)
-		sample_idx = this_sample - 1
-
-		region_rematch = region_regex.match(this_region)
-		region_idx = int(region_rematch.group(1)) - 1
-
-		# Put the data into the return data structure
-		ret_data[region_idx, sample_idx, year_idx] = this_sle * 10.0  # Convert cm to mm
-
-	# Done
-	return(ret_data, targyears)
-
-
-def emulandice_project(pipeline_id, icesource):
-
-	# Load the preprocessed data
-	preprocess_file = "{}_preprocess.pkl".format(pipeline_id)
-	with open(preprocess_file, 'rb') as f:
-		preprocess_data = pickle.load(f)
-
-	preprocess_infile = preprocess_data["infile"]
-	baseyear = preprocess_data["baseyear"]
-	scenario = preprocess_data["scenario"]
-	nsamps = preprocess_data["nsamps"]
-
-	# Load the fit data
-	fit_file = "{}_fit.pkl".format(pipeline_id)
-	with open(fit_file, 'rb') as f:
-		fit_data = pickle.load(f)
-
-	trend_mean = fit_data["trend_mean"]
-	trend_sd = fit_data["trend_sd"]
-
+def emulandice_project(pipeline_id, ice_source, region, emu_name, climate_data_file, scenario, nsamps, baseyear, 
+					   seed, pyear_start, pyear_end, pyear_step):
 
 	# Run the module using the FACTS forcing data
+	subprocess.run(["bash", "emulandice_steer.sh", ice_source, region, emu_name, climate_data_file, scenario])
 
-	py_working_dir = os.getcwd()
-	emulandice_dataset = 'FACTS_CLIMATE_FORCING.csv'
-	subprocess.run(["bash", "emulandice_steer.sh", emulandice_dataset, str(nsamps), icesource])
+	## MAYBE ABLE TO DO THIS WITHIN RPY2 WITH SOMETHING LIKE:
 
+	# from rpy2.robjects import r
+	# from rpy2.robjects.packages import importr
+
+	# # Init the packrat environment
+	# r.setwd('/path/to/packrat/environment') ## This should be the task directory
+	# r.source('.Rprofile')
+
+	# # use the packages it contains
+	# importr('emulandice2')    
+	# result = r.main()
+
+	# BUT PERHAPS THEN NEED TO CALL emulandice_environment.sh before calling emulandice_project.py
+	# THIS WOULD HAVE THE ADVANTAGE THAT r.main() could pass output as a return value rather
+	# than having to write to a file and then read it back in if we want to do checks on it in this script
+
+	## NEED TO MODIFY CHECKS SO THAT WORK WITH NETCDF OUTPUT
 
 	# Get the output from the emulandice run
-	emulandice_file = os.path.join(os.path.dirname(__file__),"results", "projections_FAIR_FACTS.csv")
-	samples, targyears = ExtractProjections(emulandice_file)
+	# samples, targyears = ExtractProjections(emulandice_file)
 
 	# Make sure we get the number of samples we expected
-	if nsamps != samples.shape[1]:
-		raise Exception("Number of SLC projections does not match number of temperature trajectories: {} != {}".format(samples.shape[1], nsamps))
+	#if nsamps != samples.shape[1]:
+	#	raise Exception("Number of SLC projections does not match number of temperature trajectories: {} != {}".format(samples.shape[1], nsamps))
 
-	# Find the ratio of melt over the first decade of projection years
-	syear_idx = 0
-	eyear_idx = np.max(np.flatnonzero(targyears <= (targyears[syear_idx]+10)))
-
-	region_melt = []
-	total_melt = 0.0
-	for x in np.arange(samples.shape[0]):
-		this_melt = np.nanmean(samples[x,:,eyear_idx]) - np.nanmean(samples[x,:,syear_idx])
-		total_melt += this_melt
-		region_melt.append(this_melt)
-	region_melt = np.array(region_melt)
-	melt_ratio = region_melt / total_melt
-
-	# Generate samples for trends correlated among ice sources
-	# Note: Keep seed hard-coded and matched with AIS and GrIS module within emulandice module set
-	rng = np.random.default_rng(8071)
-	trend_q = rng.random(nsamps)
-	glac_trend = norm.ppf(trend_q, trend_mean, trend_sd) * (targyears[syear_idx] - baseyear)
-
-	# Apply the trends for the baseline adjustment
-	for x in np.arange(samples.shape[0]):
-		this_trend = glac_trend * melt_ratio[x]
-		samples[x,:,:] += this_trend[:, np.newaxis]
-
-	# Save the global projections to a pickle
-	output = {"gic_samps": samples, "targyears": targyears, "scenario": scenario, \
-			"baseyear": baseyear, "preprocess_infile": preprocess_infile}
-	outfile = open(os.path.join(os.path.dirname(__file__), "{}_projections.pkl".format(pipeline_id)), 'wb')
-	pickle.dump(output, outfile, protocol=-1)
-	outfile.close()
-
-	# Write the global projections to netcdf files
-	gic_global_slr = np.sum(samples, axis=0)
-	WriteNetCDF(gic_global_slr, None, targyears, baseyear, scenario, nsamps, pipeline_id)
-
-	# output projections for each region as netcdf
-	for i in np.arange(len(samples)):
-		WriteNetCDF(samples[i],"glac{0}".format(i+1),targyears,baseyear,scenario,nsamps,pipeline_id)
-
-	# Done
-	return(None)
-
-
-def WriteNetCDF(slr, region, targyears, baseyear, scenario, nsamps, pipeline_id):
-
-	# Write the total global projections to a netcdf file
-	if region is None:
-		nc_filename = os.path.join(os.path.dirname(__file__), "{}_globalsl.nc".format(pipeline_id))
-		nc_description = "Global SLR contribution from glaciers using the emulandice module"
-	else:
-		nc_filename = os.path.join(os.path.dirname(__file__), "{}_{}_globalsl.nc".format(pipeline_id, region))
-		nc_description = "Global SLR contribution from glaciers ({}) using the emulandice module".format(region)
-	rootgrp = Dataset(nc_filename, "w", format="NETCDF4")
-
-	# Define Dimensions
-	year_dim = rootgrp.createDimension("years", len(targyears))
-	samp_dim = rootgrp.createDimension("samples", nsamps)
-	loc_dim = rootgrp.createDimension("locations", 1)
-
-	# Populate dimension variables
-	year_var = rootgrp.createVariable("years", "i4", ("years",))
-	samp_var = rootgrp.createVariable("samples", "i8", ("samples",))
-	loc_var = rootgrp.createVariable("locations", "i8", ("locations",))
-	lat_var = rootgrp.createVariable("lat", "f4", ("locations",))
-	lon_var = rootgrp.createVariable("lon", "f4", ("locations",))
-
-	# Create a data variable
-	samps = rootgrp.createVariable("sea_level_change", "f4", ("samples", "years", "locations"), zlib=True, complevel=4)
-
-	# Assign attributes
-	rootgrp.description = nc_description
-	rootgrp.history = "Created " + time.ctime(time.time())
-	rootgrp.source = "FACTS: {0}. ".format(pipeline_id)
-	rootgrp.baseyear = baseyear
-	rootgrp.scenario = scenario
-	samps.units = "mm"
-
-	# Put the data into the netcdf variables
-	year_var[:] = targyears
-	samp_var[:] = np.arange(nsamps)
-	samps[:,:,:] = slr[:,:,np.newaxis]
-	lat_var[:] = np.inf
-	lon_var[:] = np.inf
-	loc_var[:] = -1
 
 	return(None)
-
-
 
 if __name__ == "__main__":
 
@@ -213,28 +43,26 @@ if __name__ == "__main__":
 	epilog="Note: This is meant to be run as part of the Framework for the Assessment of Changes To Sea-level (FACTS)")
 
 	# Add arguments for the resource and experiment configuration files
+	parser.add_argument('--pipeline_id', help="Unique identifier for this instance of the module", required=True)
 	parser.add_argument('--ice_source', help="Ice source: GIS, AIS or GLA", default='AIS', choices=['AIS','GIS','GLA'])
 	parser.add_argument('--region', help="Ice source region: ALL for GIS/AIS and RGI01-RGI19 for GLA", default='ALL')
 	parser.add_argument('--emu_name', help="models_emulator_settings: e.g. CISM_pow_exp_20, CISM_IMAUICE_GISM_pow_exp_20")
-	parser.add_argument('--pipeline_id', help="Unique identifier for this instance of the module", required=True)
- 	parser.add_argument('--scenario', help="SSP Emissions scenario", required=True)
+ 	parser.add_argument('--scenario', help="SSP Emissions scenario", default='ssp245')
 	parser.add_argument('--climate_data_file', help="NetCDF4/HDF5 file containing surface temperature data", type=str)
 	parser.add_argument('--nsamps', help="Number of samples to generate [default=20000]", default=20000, type=int)
 	parser.add_argument('--seed', help="Seed value for random number generator [default=1234]", default=1234, type=int)
-	parser.add_argument('--pipeline_id', help="Unique identifier for this instance of the module", required=True)
-	parser.add_argument('--models', help="List of model names to include in the sampling process", nargs="+", default=None)
 	parser.add_argument('--pyear_start', help="Year for which projections start [default=2000]", default=2020, type=int)
 	parser.add_argument('--pyear_end', help="Year for which projections end [default=2300]", default=2300, type=int)
 	parser.add_argument('--pyear_step', help="Step size in years between pyear_start and pyear_end at which projections are produced [default=10]", default=10, type=int)
 	parser.add_argument('--baseyear', help="Base year to which slr projections are centered", type=int, default=2005)
-	parser.add_argument('--cyear_start', help="Constant rate calculation for projections starts at this year", default=None, type=int)
-	parser.add_argument('--cyear_end', help="Constant rate calculation for projections ends at this year", default=None, type=int)
 
 	# Parse the arguments
 	args = parser.parse_args()
 
 	# Run the preprocessing
-	emulandice_project(args.pipeline_id)
+	emulandice_project(args.pipeline_id, args.ice_source, args.region, args.emu_name, args.climate_data_file, 
+					   args.scenario, args.nsamps, args.baseyear, 
+					   args.seed, args.pyear_start, args.pyear_end, args.pyear_step)
 
 	# Done
 	sys.exit()
