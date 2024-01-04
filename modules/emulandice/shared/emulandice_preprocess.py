8f3607de72059376e3985c8f1e52a02862d4455b
Bob Kopp <robert.kopp@rutgers.edu>
2024-01-04 11:13:09 -0500

    first attempt at switching emulandice to the standard climate.nc file

--- modules/emulandice/shared/emulandice_preprocess.py
+++ modules/emulandice/shared/emulandice_preprocess.py
@@ -1,37 +1,45 @@
 import numpy as np
+import argparse
+import h5py
 import os
 import sys
-import fnmatch
-import argparse
-import re
 import pickle
 import shutil
-from netCDF4 import Dataset
 
 
+def GetSATData(fname, scenario, refyear_start=1850, refyear_end=1900, year_start=1900, year_end=2300):
 
-def GetSamples(ncfile, years, baseyear):
+	# Open the file
+	df_ssp = h5py.File(fname,'r')
 
-	# Load the nc file
-	with Dataset(ncfile, "r") as nc:
+	# Extract the surface temperature for this scenario
+	if scenario not in df_ssp.keys():
+		raise Except("Scenario {} not found in {}".format(scenario, fname))
+	ssp_folder = df_ssp.get(scenario)
+	sat_ssp = ssp_folder.get('surface_temperature')
 
-		# Extract the variables
-		ncyears = nc.variables["years"][...]
-		samples = np.squeeze(nc.variables["surface_temperature"][...])
-		scenario = nc.getncattr("Scenario")
+	# Get the number of ensemble members from the data
+	_,nens = sat_ssp.shape
 
-	# Extract the reference value
-	baseyear_idx = np.flatnonzero(ncyears == baseyear)
-	ref_val = samples[:,baseyear_idx]
+	# Extract the years available
+	sat_years = df_ssp['year'][()]
 
-	# Indices of years to extract and return
-	year_idx = np.flatnonzero(np.isin(ncyears, years))
+	# Which indices align with the reference and trim years
+	refyear_start_idx = np.flatnonzero(sat_years == refyear_start)[0]
+	refyear_end_idx = np.flatnonzero(sat_years == refyear_end)[0] #+ 1
+	year_start_idx = np.flatnonzero(sat_years == year_start)[0]
+	year_end_idx = np.flatnonzero(sat_years == year_end)[0] + 1
 
-	# Squeeze out the location dimension (should be global temperature trajectories)
-	samples = samples[:,year_idx] - ref_val
+	# Normalize and crop temperature series
+	Time = np.arange(year_start, year_end+1)
+	SATave = np.mean(sat_ssp[refyear_start_idx:refyear_end_idx,:], axis=0)
+	SAT = sat_ssp[year_start_idx:year_end_idx,:] - SATave
 
-	# Return the samples
-	return(samples, scenario)
+	# Close the h5 file
+	df_ssp.close()
+
+	# Done
+	return(SAT, Time, nens)
 
 
 def WriteToCSV(outfile, samples, mode="w"):
@@ -53,40 +61,39 @@ def WriteToCSV(outfile, samples, mode="w"):
 	return(None)
 
 
-def emulandice_preprocess(infile, baseyear, pipeline_id):
-
-	# If no input file was passed, look for one produced by a pre-projection workflow
-	if infile is None:
-		indir = os.path.dirname(__file__)
-		if indir == "":
-			indir = "."
-		files = fnmatch.filter(os.listdir(indir), "*_gsat.nc")
-		if files is None:
-			raise Exception("Unable to find a usable input file")
-		infile = files[0]
+def emulandice_preprocess(pipeline_id, scenario, fname, baseyear, pipeline_id):
 
 	# Years
 	years = np.arange(2015,2101)
 
-	# Get the samples
-	samps, scenario = GetSamples(infile, years, baseyear)
+	# Load the temperature data
+	samps,Time,nsamps = GetSATData(fname, scenario)
+	Tlen = len(Time)
 
-	# How many samples are we running?
-	nsamps = samps.shape[0]
+	# emulandice-specific adjustment
+
+	# Extract the reference value
+	baseyear_idx = np.flatnonzero(Time == baseyear)
+	ref_val = samps[baseyear_idx,:]
+
+	# Indices of years to extract and return
+	year_idx = np.flatnonzero(np.isin(Time, years))
+
+	# Squeeze out the location dimension (should be global temperature trajectories)
+	samps = np.transpose(samps[year_idx,:] - ref_val)
 
 	# Append these samples to the output file
 	headfile =  os.path.join(os.path.dirname(__file__), "FACTS_CLIMATE_FORCING.csv.head")
 	outfile = os.path.join(os.path.dirname(__file__), "FACTS_CLIMATE_FORCING.csv")
 	shutil.copyfile(headfile,outfile)
-	WriteToCSV(outfile, samps, mode="a")
+	WriteToCSV(outfile, np.samps, mode="a")
 
 	# Save the preprocessed data to a pickle
-	output = {"scenario": scenario, "baseyear": baseyear, "infile": infile, \
-			"facts_data_file": outfile, "nsamps": nsamps}
+	output = {"SAT": SAT, "Time": Time, "NumTensemble": NumTensemble, "Tlen": Tlen, "scenario": scenario, \
+		    "baseyear": baseyear, "infile": fname, "facts_data_file": outfile,  "nsamps": nsamps }
 	outfile = open(os.path.join(os.path.dirname(__file__), "{}_preprocess.pkl".format(pipeline_id)), 'wb')
-	pickle.dump(output, outfile, protocol=-1)
+	pickle.dump(output, outfile)
 	outfile.close()
-
 
 	# Done
 	return(None)
@@ -100,14 +107,15 @@ if __name__ == "__main__":
 
 	# Add arguments for the resource and experiment configuration files
 	parser.add_argument('--pipeline_id', help="Unique identifier for this instance of the module", required=True)
-	parser.add_argument('--input_data_file', help="Full path for temperature trajectory input file", default=None)
+	parser.add_argument('--scenario', help="SSP Emissions scenario", required=True)
+	parser.add_argument('--climate_data_file', help="NetCDF4/HDF5 file containing surface temperature data", type=str)
 	parser.add_argument('--baseyear', help="Year to which projections should be referenced", type=int, default=2005)
 
 	# Parse the arguments
 	args = parser.parse_args()
 
 	# Run the preprocessing
-	emulandice_preprocess(args.input_data_file, args.baseyear, args.pipeline_id)
+	emulandice_preprocess(args.pipeline_id, args.scenario, args.climate_data_file, args.baseyear )
 
 	# Done
 	sys.exit()
