bc8e44be353b5c3210389cab3e480356dd56bf3e
QadisChaudhry <qadis.chaudhry@gmail.com>
2024-03-29 15:59:14 -0400

    added memory files using kubernetes

--- fairtest/execute.sh
+++ fairtest/execute.sh
@@ -9,15 +9,17 @@ tar -xvf ./modules-data/fair_temperature_preprocess_data.tgz
         # rm /modules-data/fair_temperature_preprocess_data.tgz || rp_error pre_exec
 # pip install --upgrade pip; pip install numpy scipy netCDF4 pyyaml matplotlib pandas xarray dask fair==1.6.4 memory-profiler || rp_error pre_exec
 
-python3 fair_temperature_preprocess.py "--scenario" "ssp585" "--pipeline_id" "configTest.temperature.fair.temperature"
+# python3 fair_temperature_preprocess.py "--scenario" "ssp585" "--pipeline_id" "configTest.temperature.fair.temperature"
+mprof run -o mem_fair_preprocess.dat fair_temperature_preprocess.py "--scenario" "ssp585" "--pipeline_id" "configTest.temperature.fair.temperature"
 
 tar -xvf ./modules-data/fair_temperature_fit_data.tgz
-python3 fair_temperature_fit.py "--pipeline_id" "configTest.temperature.fair.temperature"
+# python3 fair_temperature_fit.py "--pipeline_id" "configTest.temperature.fair.temperature"
+mprof run -o mem_fair_fit.dat fair_temperature_fit.py "--pipeline_id" "configTest.temperature.fair.temperature"
 
+# python3 fair_temperature_project.py "--pipeline_id" "configTest.temperature.fair.temperature" "--nsamps" "2000"
+mprof run -o mem_fair_project.dat fair_temperature_project.py "--pipeline_id" "configTest.temperature.fair.temperature" "--nsamps" "2000"
 
-python3 fair_temperature_project.py "--pipeline_id" "configTest.temperature.fair.temperature" "--nsamps" "2000"
-
-
-python3 fair_temperature_postprocess.py "--pipeline_id" "configTest.temperature.fair.temperature"
+# python3 fair_temperature_postprocess.py "--pipeline_id" "configTest.temperature.fair.temperature"
+mprof run -o mem_fair_postprocess.dat fair_temperature_postprocess.py "--pipeline_id" "configTest.temperature.fair.temperature"
 
 ls -la
--- larmiptest/execute.sh
+++ larmiptest/execute.sh
@@ -7,22 +7,26 @@ tar -xvf ./modules-data/ipccar6_climate_data.tgz
 
 
 
-python3 larmip_icesheet_preprocess.py "--scenario" "ssp585" "--pipeline_id" "configTest.larmip.larmip.AIS"
+# python3 larmip_icesheet_preprocess.py "--scenario" "ssp585" "--pipeline_id" "configTest.larmip.larmip.AIS"
+mprof run mem_larmip_preprocess.dat larmip_icesheet_preprocess.py "--scenario" "ssp585" "--pipeline_id" "configTest.larmip.larmip.AIS"
 
 echo "=== larmip preprocess step COMPLETE ==="
 
 tar -xvf ./modules-data/larmip_icesheet_fit_data.tgz
-python3 larmip_icesheet_fit.py "--pipeline_id" "configTest.larmip.larmip.AIS"
+# python3 larmip_icesheet_fit.py "--pipeline_id" "configTest.larmip.larmip.AIS"
+mprof run -o mem_larmip_fit.dat larmip_icesheet_fit.py "--pipeline_id" "configTest.larmip.larmip.AIS"
 
 echo "=== larmip fit step COMPLETE ==="
 
 tar -xvf ./modules-data/larmip_icesheet_project_data.tgz
-python3 larmip_icesheet_project.py "--nsamps" "2000" "--baseyear" "2005" "--pyear_start" "2020" "--pyear_end" "2150" "--pyear_step" "10" "--pipeline_id" "configTest.larmip.larmip.AIS"
+# python3 larmip_icesheet_project.py "--nsamps" "2000" "--baseyear" "2005" "--pyear_start" "2020" "--pyear_end" "2150" "--pyear_step" "10" "--pipeline_id" "configTest.larmip.larmip.AIS"
+mprof run -o mem_larmip_project.dat larmip_icesheet_project.py "--nsamps" "2000" "--baseyear" "2005" "--pyear_start" "2020" "--pyear_end" "2150" "--pyear_step" "10" "--pipeline_id" "configTest.larmip.larmip.AIS"
 
 echo "=== larmip project step COMPLETE ==="
 
 tar -xvf ./modules-data/grd_fingerprints_data.tgz
-python3 larmip_icesheet_postprocess.py "--pipeline_id" "configTest.larmip.larmip.AIS"
+# python3 larmip_icesheet_postprocess.py "--pipeline_id" "configTest.larmip.larmip.AIS"
+mprof run -o mem_larmip_postprocess.dat larmip_icesheet_postprocess.py "--pipeline_id" "configTest.larmip.larmip.AIS"
 
 echo "=== larmip postprocess step COMPLETE ==="
 
