67ca1ceb9fd850c579b0e5d6d9f094152b1f0719
QadisChaudhry <qadis.chaudhry@gmail.com>
2024-03-30 15:58:35 -0400

    add tlm to kubernetes (needs run)

--- tlmtest/execute.sh
+++ tlmtest/execute.sh
@@ -7,19 +7,25 @@ tar -xvf ./modules-data/tlm_sterodynamics_preprocess_data.tgz
 tar -xvf ./modules-data/tlm_sterodynamics_cmip6_data.tgz
 tar -xvf ./modules-data/ipccar6_climate_data.tgz
 
-python3 tlm_sterodynamics_preprocess.py "--scenario" "ssp585" "--baseyear" "2005" "--pyear_start" "2020" "--pyear_end" "2150" "--pyear_step" "10" "--pipeline_id" "configTest.ocean.tlm.sterodynamics"
+# python3 tlm_sterodynamics_preprocess.py "--scenario" "ssp585" "--baseyear" "2005" "--pyear_start" "2020" "--pyear_end" "2150" "--pyear_step" "10" "--pipeline_id" "configTest.ocean.tlm.sterodynamics"
+mprof run -o mem_tlm_preprocess.dat tlm_sterodynamics_preprocess.py "--scenario" "ssp585" "--baseyear" "2005" "--pyear_start" "2020" "--pyear_end" "2150" "--pyear_step" "10" "--pipeline_id" "configTest.ocean.tlm.sterodynamics"
 
 echo "=== tlm/sterodynamics preprocess complete ==="
 
-python3 tlm_sterodynamics_fit.py "--pipeline_id" "configTest.ocean.tlm.sterodynamics"
+# python3 tlm_sterodynamics_fit.py "--pipeline_id" "configTest.ocean.tlm.sterodynamics"
+mprof run -o mem_tlm_fit.dat tlm_sterodynamics_fit.py "--pipeline_id" "configTest.ocean.tlm.sterodynamics"
 
 echo "=== tlm/sterodynamics fit complete ==="
 
-python3 tlm_sterodynamics_project.py "--pipeline_id" "configTest.ocean.tlm.sterodynamics" "--nsamps" "2000"
+# python3 tlm_sterodynamics_project.py "--pipeline_id" "configTest.ocean.tlm.sterodynamics" "--nsamps" "2000"
+mprof run -o mem_tlm_project.dat tlm_sterodynamics_project.py "--pipeline_id" "configTest.ocean.tlm.sterodynamics" "--nsamps" "2000"
 
 echo "=== tlm/sterodynamics project complete ==="
 
-python3 tlm_sterodynamics_postprocess.py "--nsamps" "2000" "--pipeline_id" "capstone.modules.ocean.tlm.sterodynamics"
+# python3 tlm_sterodynamics_postprocess.py "--nsamps" "2000" "--pipeline_id" "capstone.modules.ocean.tlm.sterodynamics"
+mprof run -o mem_tlm_postprocess.dat tlm_sterodynamics_postprocess.py "--nsamps" "2000" "--pipeline_id" "configTest.ocean.tlm.sterodynamics"
 
 echo "=== tlm/sterodynamics postprocess complete ==="
 echo "=== tlm/sterodynamics complete ==="
+
+ls -la
