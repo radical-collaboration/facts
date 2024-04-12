e4ec84870cee84c153cf37da6f71237e5e377d8e
QadisChaudhry <qadis.chaudhry@gmail.com>
2024-04-12 17:40:35 -0400

    global kubernetes

--- global.sh
+++ global.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sed "s/whoami/$(whoami)/g; s+homedir+$(echo $HOME)+g" kubernetes.yaml > temp_file.yaml && mv temp_file.yaml kubernetes.yaml
+sed "s/whoami/$(whoami)/g; s+homedir+$(echo $HOME)+g" global_kubernetes.yaml > temp_file.yaml && mv temp_file.yaml kubernetes.yaml
 
 find *test -name "develop.sh" -exec sh {} \;
 
