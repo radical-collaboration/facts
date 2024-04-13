0756fe7f0bca9213dff5454d5adae0e8bfc855f0
QadisChaudhry <qadis.chaudhry@gmail.com>
2024-04-13 13:18:46 -0400

    minor changes

--- global.sh
+++ global.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+find totaltest/to_total -type f -delete
+cp totaltest/configTest.ocean.tlm.sterodynamics_globalsl.nc totaltest/to_total/global/
+cp totaltest/configTest.ocean.tlm.sterodynamics_localsl.nc totaltest/to_total/local/
+
 sed "s/whoami/$(whoami)/g; s+homedir+$(echo $HOME)+g" global_kubernetes.yaml > temp_file.yaml && mv temp_file.yaml kubernetes.yaml
 
 find *test -name "develop.sh" -exec sh {} \;
