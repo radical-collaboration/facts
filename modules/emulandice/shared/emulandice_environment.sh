0ff9a848c68040d1e939bf268095deab8c1061e8
pkjr002 <pkjr002@gmail.com>
2025-03-19 23:27:58 -0400

    Update: radical  & R version

--- modules/emulandice/shared/emulandice_environment.sh
+++ modules/emulandice/shared/emulandice_environment.sh
@@ -11,7 +11,7 @@ if [ ${hs: -18} = 'amarel.rutgers.edu' ]; then
 
     module purge
     module load intel/19.0.3
-    module load R/3.6.3-gc563
+    module load R/4.1.0-gc563
     module load gcc/7.3.0-gc563
     module load cmake/3.24.3-sw1088
 
